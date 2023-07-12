418f085ab4f2b5dc256a2696f5bb88d001273177
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2023-07-12 08:51:06 -0500

    render white cube

--- meshes/base_mesh.py
+++ meshes/base_mesh.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class BaseMesh:
     def __init__(self) -> None:
         # OpenGL context
@@ -19,9 +20,7 @@ class BaseMesh:
         vertex_data = self.get_vertex_data()
         vbo = self.ctx.buffer(vertex_data)
         vao = self.ctx.vertex_array(
-            self.program, 
-            [(vbo, self.vbo_format, *self.attrs)], 
-            skip_errors=True
+            self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors=True
             )
         return vao
     
